a4cf49b693a348660e7c8f473f5cf1f60afc3ceb
Shivam kumar <shivamsharma27107@gmail.com>
2025-07-24 00:08:28 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-   '/hr': 'http://localhost:4000',
+   '/hr': 'https://neorecruiter-ai-hr.onrender.com',
     strictPort: true
   },
 })
